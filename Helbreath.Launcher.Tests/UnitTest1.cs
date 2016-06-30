9f1e568b4ecb783601d792c9369fe6f6bfaa44c1
pawelsawicz <pawel@sawicz.eu>
2016-07-01 00:49:27 +0100

    add base tests, for versioning, downloading files

---
+++ Helbreath.Launcher.Tests/UnitTest1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Extensions;
+
+namespace Helbreath.Launcher.Tests
+{
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Two_Versions_Then_Check_If_Newer_ReturnTrue()
+        {
+            // arrange
+            var oldVersion = 0.1;
+            var newVersion = 0.2;
+            var isNewerFiles = true;
+
+            // act
+            var result = this.CheckVersion(newVersion, oldVersion);
+
+            // assert
+            Assert.AreEqual(isNewerFiles, result);
+        }
+
+        [Test]
+        public void Two_Versions_Then_Check_If_Newer_ReturnFalse()
+        {
+            //arrange
+            var oldVersion = 0.2;
+            var newVersion = 0.2;
+            var isNewerFiles = false;
+
+            //act
+            var result = this.CheckVersion(newVersion, oldVersion);
+
+            //assert
+            Assert.AreEqual(isNewerFiles, result);
+        }
+
+        [Test]
+        public void Send_Request_To_Website_Then_Return_ReturnLastesVersion()
+        {
+            //arrange
+            Mock<RestClient> restclientMock = new Mock<RestClient>();
+            restclientMock.Setup(x => x.Execute(It.IsAny<IRestRequest>())).Returns(new RestResponse
+            {
+                Content = "{ 'version' : 1 }"
+            });
+            var restclient = restclientMock.Object;
+
+            //act
+            var result = restclient.Get(new RestRequest());
+
+            var resultVersion = JsonConvert.DeserializeObject<GameVersion>(result.Content);
+
+            //assert
+            Assert.AreEqual("{ 'version' : 1 }", result.Content);
+            Assert.AreEqual(1, resultVersion.Version);
+        }
+
+        [Test]
+        public void GetUpdatesFromWebsite_Then_Download_And_Copy_To_Root_Folder()
+        {
+            //arrange
+            var client = new RestClient("https://s3-eu-west-1.amazonaws.com/livecoding-tv/my-game-patch-0.3.txt");
+            var request = new RestRequest(Method.GET);
+
+            //act
+            client.DownloadData(request).SaveAs("C:/my-game-patch-0.3.txt");
+            var updateExist = File.Exists("C:/my-game-patch-0.3.txt");
+
+            //assert
+            Assert.That(updateExist);
+        }
+
+        [Test]
+        public void Read_Then_Read_Version_From_File_Return_CurrentVersion()
+        {
+            //arrange
+            var currentVersionProvider = new InMememoryCurrentVersionProvider();
+            var expectedVersion = 0.1;
+
+            //act
+            var result = currentVersionProvider.GetCurrentVersionFromComputer();
+
+            //assert
+            Assert.AreEqual(expectedVersion, result);
+        }
+
+        public class GameVersion
+        {
+            public int Version { get; set; }
+        }
+
+        public bool CheckVersion(double newerVersion, double oldVersion)
+        {
+            if (newerVersion > oldVersion)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Helbreath.Launcher/InMememoryCurrentVersionProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helbreath.Launcher
+{
+    public class InMememoryCurrentVersionProvider : ICurrentVersionProvider
+    {
+        public double GetCurrentVersionFromComputer()
+        {
+            return 0.1;
+        }
+    }
+}
